3a47005ad12dedb5f92fd45e1ed45dd434878718
mercedesmagnelli <mechamagnelli@gmail.com>
2022-07-24 19:42:09 -0300

    inicial

---
+++ src/test/java/parcial1erCuatri/Disenio/DisenioApplicationTests.java
@@ -0,0 +1,13 @@
+package parcial1erCuatri.Disenio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisenioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
