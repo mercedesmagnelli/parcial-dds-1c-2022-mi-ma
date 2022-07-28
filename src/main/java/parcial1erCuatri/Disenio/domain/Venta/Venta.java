f473302573ca9270cd5a2768e578b26d3acb6b26
Ckanerloy <camilakanerloy@gmail.com>
2022-07-28 17:42:16 -0300

    Persistencia de venta

--- src/main/java/parcial1erCuatri/Disenio/domain/Venta/Venta.java
+++ src/main/java/parcial1erCuatri/Disenio/domain/Venta/Venta.java
@@ -1,16 +1,27 @@
 package parcial1erCuatri.Disenio.domain.Venta;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
 
+@Entity
 public class Venta{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	@OneToMany
+	@JoinColumn(name = "ordenDeCompra_id")
 	private Collection<ItemVenta> itemsVenta;
 	private MedioDePago medioDePago;
     private LocalDate fechaDeVenta;
 	//Una vez que se termina de hacer la ordenDeCompra en el carrito y se la paga (Aceptandola). Se hace un new a OrdenDeCompra con todos los atributos que tenga el carrito y se agrega el precioTotalConDescuento y precioTotalSinDescuento de los métodos de carrito
 	private double precioTotalSinDescuento;
 	private double precioTotalConDescuento;
+
+	public Venta(){
+		super();
+	}
 
 	public Venta(
             LocalDate fechaDeVenta, MedioDePago medioDePago, double precioTotalSinDescuento , double precioTotalConDescuento) {
@@ -52,5 +63,28 @@ public class Venta{
 	public void setPrecioTotalConDescuento(double precioTotalConDescuento) {
 		this.precioTotalConDescuento = precioTotalConDescuento;
 	}
-	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Collection<ItemVenta> getItemsVenta() {
+		return itemsVenta;
+	}
+
+	public void setItemsVenta(Collection<ItemVenta> itemsVenta) {
+		this.itemsVenta = itemsVenta;
+	}
+
+	public LocalDate getFechaDeVenta() {
+		return fechaDeVenta;
+	}
+
+	public void setFechaDeVenta(LocalDate fechaDeVenta) {
+		this.fechaDeVenta = fechaDeVenta;
+	}
 }
